4eec4c4776b05f9a916f045aad4891db4f8ed4e6
melanie-attard <melattard15@gmail.com>
2023-04-17 14:09:08 +0200

    Add tab layout in show details activity

--- app/src/main/java/com/nowshowing/ShowDetailsActivity.java
+++ app/src/main/java/com/nowshowing/ShowDetailsActivity.java
@@ -1,6 +1,8 @@
 package com.nowshowing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,21 +10,53 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.tabs.TabLayout;
+import com.nowshowing.detailsFragments.VPAdapter;
 import com.squareup.picasso.Picasso;
 
 public class ShowDetailsActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private VPAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_details);
 
+        // set up tab layout
+        // adapted from https://www.youtube.com/watch?v=EukutTtf9tQ
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager2 = findViewById(R.id.viewpager);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new VPAdapter(fragmentManager, getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+        });
+
+        // change fragments on slide
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
         // get Id from intent
         Intent intent = getIntent();
         int Id = intent.getIntExtra("Id", 0);
-
-        TextView txt = (TextView) findViewById(R.id.txt_hello);
-        txt.setText("Selected: " + Id);
 
         // get image from intent
         ImageView imageView = findViewById(R.id.imageView);
---
+++ app/src/main/java/com/nowshowing/detailsFragments/EpisodesFragment.java
@@ -0,0 +1,20 @@
+package com.nowshowing.detailsFragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nowshowing.R;
+
+public class EpisodesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_episodes, container, false);
+    }
+}
