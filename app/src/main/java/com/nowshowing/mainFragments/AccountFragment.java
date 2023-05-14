193269f97e56230e2ede5ff1c9c55c38eec44e28
melanie-attard <melattard15@gmail.com>
2023-05-14 10:20:45 +0200

    Added login activity UI

--- app/src/main/java/com/nowshowing/mainFragments/AccountFragment.java
+++ app/src/main/java/com/nowshowing/mainFragments/AccountFragment.java
@@ -1,23 +1,38 @@
 package com.nowshowing.mainFragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.nowshowing.R;
 import com.nowshowing.databinding.FragmentAccountBinding;
+import com.nowshowing.mainFragments.account.LoginActivity;
 
 public class AccountFragment extends Fragment {
 
+    Button login;
+    Button register;
     private FragmentAccountBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentAccountBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        login = root.findViewById(R.id.login_btn);
+        register = root.findViewById(R.id.register_btn);
+
+        // set an on-click listener for each button
+        login.setOnClickListener(view -> {
+            Intent intent = new Intent(root.getContext(), LoginActivity.class);
+            startActivity(intent);
+        });
+        
         return root;
     }
 
