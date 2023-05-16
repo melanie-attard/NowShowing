8ef694caa36a8c0a67d5cbe4b4ef8d8491fa05b8
melanie-attard <melattard15@gmail.com>
2023-05-16 08:47:07 +0200

    Maintaining the logged in user within shared preferences & updating the drawer layout

--- app/src/main/java/com/nowshowing/MainActivity.java
+++ app/src/main/java/com/nowshowing/MainActivity.java
@@ -1,13 +1,21 @@
 package com.nowshowing;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.MenuItem;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.core.view.MenuItemCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -22,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    SharedPreferences sharedPref;
+    private String user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +43,63 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(binding.appBarMain.toolbar);
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
+
+        // get text view in drawer header
+        View header = navigationView.getHeaderView(0);
+        TextView username = header.findViewById(R.id.current_username);
+
+        // retrieve shared preferences containing the current user
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        // refreshing the drawer layout everytime it is opened,
+        // adapted from https://itecnote.com/tecnote/android-refresh-header-in-navigation-drawer/
+        ActionBarDrawerToggle DrawerToggle = new ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(R.string.title_activity_main);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(null);
+
+                // get logged in user (default value is null)
+                user = sharedPref.getString(getString(R.string.current_user_key), null);
+                if(user != null) {
+                    // set drawer header username, if a user is logged in
+                    username.setText(user);
+                }
+
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        drawer.addDrawerListener(DrawerToggle);
+        DrawerToggle.setDrawerIndicatorEnabled(true);
+
+        // setup of log out button
+        Button logout = navigationView.findViewById(R.id.btn_Logout);
+        logout.setOnClickListener(view -> {
+            // get logged in user (default value is null)
+            user = sharedPref.getString(getString(R.string.current_user_key), null);
+            if(user != null){
+                // set current user to null
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString(getString(R.string.current_user_key), null);
+                editor.apply();
+
+                Toast.makeText(MainActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+
+                // remove username from header
+                username.setText(null);
+            }
+        } );
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -48,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
 
-        // set up the search view, this is only visible in the home fragment
+        // setting up the search view, this is only visible in the home fragment
         // inspired by https://www.geeksforgeeks.org/how-to-implement-android-searchview-with-example/
         MenuItem searchView = menu.findItem(R.id.search_bar);
         SearchView search = (SearchView) MenuItemCompat.getActionView(searchView);
--- app/src/main/java/com/nowshowing/mainFragments/account/LoginActivity.java
+++ app/src/main/java/com/nowshowing/mainFragments/account/LoginActivity.java
@@ -2,8 +2,11 @@ package com.nowshowing.mainFragments.account;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -16,10 +19,15 @@ public class LoginActivity extends AppCompatActivity {
     private UserDBHelper DB;
     private TextInputLayout username, password;
     private Button login_btn, register_now_btn;
+    SharedPreferences sharedPref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // get the shared preference file to update the logged in user,
+        // inspired by https://stackoverflow.com/questions/11316560/sharedpreferences-from-different-activity
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
         DB = new UserDBHelper(this);
         username = findViewById(R.id.username);
@@ -52,6 +60,11 @@ public class LoginActivity extends AppCompatActivity {
 
                 Boolean exists = DB.userExists(user, pass);
                 if(exists){
+                    // set the user as current user in shared preferences
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString(getString(R.string.current_user_key), user);
+                    editor.apply();
+
                     Toast.makeText(this, "Signed in successfully", Toast.LENGTH_SHORT).show();
                     // navigate user back to account fragment
                     onBackPressed();
