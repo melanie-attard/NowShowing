ce78f62c963196d91b88fe1af5b5835955c5f7f4
melanie-attard <melattard15@gmail.com>
2023-05-16 09:13:19 +0200

    Initial UI setup for FavouritesFragment

--- app/src/main/java/com/nowshowing/mainFragments/FavouritesFragment.java
+++ app/src/main/java/com/nowshowing/mainFragments/FavouritesFragment.java
@@ -1,24 +1,39 @@
 package com.nowshowing.mainFragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.nowshowing.R;
 import com.nowshowing.databinding.FragmentFavouritesBinding;
 
 public class FavouritesFragment extends Fragment {
 
     private FragmentFavouritesBinding binding;
+    SharedPreferences sharedPref;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        //GalleryViewModel galleryViewModel = new ViewModelProvider(this).get(GalleryViewModel.class);
-
         binding = FragmentFavouritesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        TextView msg = root.findViewById(R.id.no_login_msg);
+        // retrieve shared preferences containing the current user
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(root.getContext().getApplicationContext());
+        String user = sharedPref.getString(getString(R.string.current_user_key), null);
+        if(user == null){
+            // if the user is not logged in, they cannot view their favourites
+            msg.setVisibility(View.VISIBLE);
+        }
+        else{
+            msg.setVisibility(View.INVISIBLE);
+        }
 
         return root;
     }
